77b336ee8dc312b2aa2c69e3291b5d3b4945fe20
mpoiitis <poetakos@gmail.com>
2016-03-11 22:13:02 +0200

    Add exercise 4 source code

--- ex4/main.py
+++ ex4/main.py
@@ -1,5 +1,30 @@
 import codecs
 
-file = codecs.open('/home/archlord/Desktop/project-1-crypto/thema4.txt','r','cp1253')
+dictionary = ['Α', 'Β', 'Γ', 'Δ', 'Ε', 'Ζ', 'Η', 'Θ', 'Ι', 'Κ', 'Λ', 'Μ', 'Ν', 'Ξ', 'Ο', 'Π', 'Ρ', 'Σ',
+              'Τ', 'Υ', 'Φ', 'Χ', 'Ψ', 'Ω']
+
+def encode(string):
+    coded_text = []
+    for letter in string:
+        coded_text.append(dictionary.index(letter))
+    return coded_text
+
+def decode(list):
+    decoded_text = []
+    for i in list:
+        decoded_text.append(dictionary[i])
+    return decoded_text
+
+file = codecs.open('thema4.txt','r','cp1253')
 string = file.read()
 print(string)
+newstring = encode(string)
+print(newstring)
+for i in range(24):
+    temp = [(x+i)%24 for x in newstring]#permutation list
+    print(''.join(decode(temp)))
+
+
+
+
+
