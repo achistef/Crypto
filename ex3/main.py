eddfa5cda7cd8eaffc5d5f1f3c9e58886f6e8fcb
achistef <achilles@outlook.com.gr>
2016-03-12 13:18:37 +0200

    exercise 3

--- ex3/main.py
+++ ex3/main.py
@@ -15,29 +15,41 @@ def decode(list):
     return decoded_text
 
 def Friedman(newstring):
-	k = 2
+	k = 7
 	found = 0
 	while found == 0:
 		rows = len(newstring)/k
 		columns = k
-		temp = [[0 for x in range(columns)] for x in range(rows)]
+	    temp = [[0 for x in range(columns)] for x in range(rows)]
 
 		for i in range (rows):
 			for j in range(columns):
-				temp[i][j] = newstring[(i*k)+j] 
+                temp[i][j] = newstring[(i*k)+j]
 
-		for i in in range(k):
-			if IC(temp[][i]) - 0.067 < 0.05:
-				found = 1
-				return k		
-		k++
+
+        axi = 0
+		for i in range (k):
+            axi = axi + IC(column(temp,i))
+			#if IC(column(temp,i)) - 0.067 < 0.05:
+				#found = 1
+				#return k
+
+        print(axi/columns)
+		k = k+1
+		found = 1
+    return k
 
 def IC(column):
 	n = 26
-	l = n
 	k = len(column)
+    sum = 0.0
+    for i in range(26):
+        x = column.count(i)
+        sum = sum + (double(x*(x-1)/k*(k-1))
+    return sum
 
-		
+def column(matrix, i):
+    return [row[i] for row in matrix]
 
 
 file = open('vigenere.txt','r')
@@ -46,6 +58,3 @@ string = file.read()
 newstring = encode(string)
 
 k = Friedman(newstring)
-
-
-
