508f155d14ec869f3d50c81ab434669d48ab4847
mpoiitis <poetakos@gmail.com>
2016-03-15 18:53:43 +0200

    Add ex8

---
+++ ex8/main.py
@@ -0,0 +1,21 @@
+import crypt
+
+file = open('password.txt','r')
+
+temp = file.read().partition(":")
+username = temp[0]
+password = temp[2].partition(":")[0]
+
+file.close()
+file = open('password.txt','r')
+salt = file.read().partition("$")[2].partition("$")[2].partition("$")[0]
+
+encryption = "$6$" + str(salt) + "$"
+for i in range(1,1000000):
+	x = '{0:06d}'.format(i) 
+	encrypted = crypt.crypt(x,encryption)
+	print(i)
+	if encrypted == password :
+		print("Password: " + str(i))
+		break
+
