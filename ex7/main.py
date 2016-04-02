6e9e97946381c926c364d6ff0708b9cf202206a6
mpoiitis <poetakos@gmail.com>
2016-04-02 13:51:07 +0300

    Update main for python 3 compatibility

--- ex7/main.py
+++ ex7/main.py
@@ -1,14 +1,14 @@
 import zipfile
 
-file = open('english.txt','r')
+myFile = open('english.txt','r')
 zFile=zipfile.ZipFile('test_zip.zip')
 
-for line in file:
+for line in myFile:
 	try:
 		password = line.strip()
-		zFile.extractall(pwd=password)
+		zFile.extractall(pwd=bytes(password,'UTF-8'))
 		print("Password: " + password)
 		break
 	except:
 		pass
-	
+		
